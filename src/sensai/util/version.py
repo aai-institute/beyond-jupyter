34d1cacac4648958e6481c57e8456879e54924f7
Dominik Jain <d.jain@appliedai.de>
2023-07-13 11:28:39 +0200

    Add util.version to facilitate version-dependent changes in dependencies

---
+++ src/sensai/util/version.py
@@ -0,0 +1,27 @@
+
+
+class Version:
+    """
+    Assists in checking the version of a Python package based on the __version__ attribute
+    """
+    def __init__(self, package):
+        """
+        :param package: the package object
+        """
+        self.components = package.__version__.split(".")
+
+    def isAtLeast(self, *components: int):
+        """
+        Checks this version against the given version components.
+        This version object must contain at least the respective number of components
+
+        :param components: version components in order (i.e. major, minor, patch, etc.)
+        :return: True if the version is at least the given version, False otherwise
+        """
+        for i, desired_min_version in enumerate(components):
+            actual_version = int(self.components[i])
+            if actual_version < desired_min_version:
+                return False
+            elif actual_version > desired_min_version:
+                return True
+        return True
