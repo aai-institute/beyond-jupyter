936f87c80e31d6627553535fcfbfb6b8f2c33531
Dominik Jain <d.jain@appliedai.de>
2023-07-13 11:29:56 +0200

    Add feature importance computation

--- scripts/eval_models.py
+++ scripts/eval_models.py
@@ -2,11 +2,15 @@ import os
 
 from pop.features import FeatureName
 from pop.models import DataSet, ModelFactory, ModelEvaluation
+from sensai import VectorRegressionModel
+from sensai.evaluation.eval_util import ModelComparisonVisitorAggregatedFeatureImportance
+from sensai.feature_importance import FeatureImportanceProvider
 from sensai.util import logging, markUsed
 from sensai.util.io import ResultWriter
 from sensai.util.logging import datetimeTag
 
 log = logging.getLogger(__name__)
+markUsed(FeatureName)
 
 
 def eval_models(dataset: DataSet, use_crossval=False):
@@ -25,8 +29,13 @@ def eval_models(dataset: DataSet, use_crossval=False):
             #ModelFactory.create_xgbrf(),
         ]
 
+        visitors = [ModelComparisonVisitorAggregatedFeatureImportance(model.getName())
+            for model in models
+            if isinstance(model, FeatureImportanceProvider) and isinstance(model, VectorRegressionModel)]
+
         ev = ModelEvaluation(dataset).create_evaluator()
-        result = ev.compareModels(models, result_writer, useCrossValidation=use_crossval)
+        result = ev.compareModels(models, result_writer, useCrossValidation=use_crossval, visitors=visitors,
+            writeVisitorResults=True)
         markUsed(result)
 
     except Exception as e:
@@ -38,5 +47,9 @@ def eval_models(dataset: DataSet, use_crossval=False):
 if __name__ == '__main__':
     logging.configureLogging()
     log.info("Starting")
-    globals().update(eval_models(DataSet(10000), use_crossval=True))
+
+    globals().update(eval_models(DataSet(10000), use_crossval=False))
+    #globals().update(eval_models(DataSet(10000), use_crossval=True))
+    #globals().update(eval_models(DataSet(None), use_crossval=False))
+
     log.info("Done")
