6896d3bcaa6a9836c4d72d2cf085976803f7cdf6
Kristof Schroeder <k.schroeder@applied-institute.de>
2023-08-08 11:27:42 +0200

    Make model factory of step 4 more excplicit

--- refactoring-journey/step04-refactoring/songpop/model_factory.py
+++ refactoring-journey/step04-refactoring/songpop/model_factory.py
@@ -11,26 +11,28 @@ from .data import COL_YEAR, COLS_MUSICAL_DEGREES, COL_KEY, COL_MODE, COL_TEMPO, 
 
 class ModelFactory:
     COLS_USED_BY_ORIGINAL_MODELS = [COL_YEAR, *COLS_MUSICAL_DEGREES, COL_KEY, COL_MODE, COL_TEMPO, COL_TIME_SIGNATURE, COL_LOUDNESS,
-        COL_DURATION_MS]
-
-    @classmethod
-    def _create_pipeline_orig_model(cls, model) -> Pipeline:
-        return Pipeline([
-            ("project_scale", ColumnTransformer([("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)])),
-            ("model", model)])
+                                    COL_DURATION_MS]
 
     @classmethod
     def create_logistic_regression_orig(cls):
-        return cls._create_pipeline_orig_model(linear_model.LogisticRegression(solver='lbfgs', max_iter=1000))
+        return Pipeline([
+            ("project_scale", ColumnTransformer([("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)])),
+            ("model", linear_model.LogisticRegression(solver='lbfgs', max_iter=1000))])
 
     @classmethod
     def create_knn_orig(cls):
-        return cls._create_pipeline_orig_model(KNeighborsClassifier(n_neighbors=1))
+        return Pipeline([
+            ("project_scale", ColumnTransformer([("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)])),
+            ("model", KNeighborsClassifier(n_neighbors=1))])
 
     @classmethod
     def create_random_forest_orig(cls):
-        return cls._create_pipeline_orig_model(RandomForestClassifier(n_estimators=100))
+        return Pipeline([
+            ("project_scale", ColumnTransformer([("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)])),
+            ("model", RandomForestClassifier(n_estimators=100))])
 
     @classmethod
     def create_decision_tree_orig(cls):
-        return cls._create_pipeline_orig_model(DecisionTreeClassifier(random_state=42, max_depth=2))
+        return Pipeline([
+            ("project_scale", ColumnTransformer([("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)])),
+            ("model", DecisionTreeClassifier(random_state=42, max_depth=2))])
