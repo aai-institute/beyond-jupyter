c4f3cc26d103fe5a6b6ecda93ea0ceaebac80659
Kristof Schroeder <k.schroeder@applied-institute.de>
2023-08-18 11:55:08 +0200

    Add README for hyperopt step, rename variables in run script

--- refactoring-journey/step10-hyperopt/README.md
+++ refactoring-journey/step10-hyperopt/README.md
@@ -1,3 +1,58 @@
 # Step 9: Hyperparameter Optimisation
 
-TODO
+While the model parameters adjust themselves based on the data and the learning algorithm, hyperparameters are set beforehand and remain static throughout the training.
+This might be the number of layers and neurons in a neural network or the maximal depth of a tree model.
+
+By adjusting these parameters, we can achieve superior performance, often resulting in models, which produce more accurate predictions. 
+The intricate relationship between hyperparameters and model performance is not always straightforward. Therefore, systematic optimization can uncover effective combinations that might be overlooked in manual experimentation.
+
+As it is always a good idea to use existing libraries, if they fit our needs, 
+we will use the well established framework [hyperopt](https://github.com/hyperopt/hyperopt/tree/master) to tune the
+hyperparameters of our xgboost model. For this, we have to define a
+search space for our hyperparameters and an objective function, which should be minimised with hyperopt.
+
+The search space is simple defined as a dictionary, containing hyperopt objects describing the desired range of parameters.
+We concentrate on parameters, related to handling over-fitting issues, for a detailed explanation of the parameters, see
+the [xgboost documentation](https://xgboost.readthedocs.io/en/stable/parameter.html#parameters-for-tree-booster).
+
+```python
+search_space = {
+            'max_depth': hp.quniform("max_depth", 3, 18, 1),
+            'gamma': hp.uniform('gamma', 0, 9),
+            'reg_lambda': hp.uniform('reg_lambda', 0, 1),
+            'colsample_bytree': hp.uniform('colsample_bytree', 0.25, 1),
+            'min_child_weight': hp.quniform('min_child_weight', 1, 12, 2),
+        }
+```
+To define the objective function, why have to use a factory method, which
+for a given element of the search space, constructs the according
+xgboost model. As we have already defined a model factory in the previous steps,
+this reduces to calling this factory with the correct entries of the search space element.
+```python
+def create_model(search_space_element: Dict[str, Any]):
+            return RegressionModelFactory.create_xgb(
+                verbosity=1,
+                max_depth=int(search_space_element['max_depth']),
+                gamma=search_space_element['gamma'],
+                reg_lambda=search_space_element['reg_lambda'],
+                min_child_weight=int(search_space_element['min_child_weight']),
+                colsample_bytree=search_space_element['colsample_bytree'])
+```
+We choose the *root relative squared error metric* (RRSE) as the value to be
+minimised. Accordingly, we can define our objective in the following way:
+```python
+io_data = dataset.load_io_data()
+metric = RegressionMetricRRSE()
+evaluator_params = VectorRegressionModelEvaluatorParams(fractional_split_test_fraction=0.3, fractional_split_random_seed=21)
+ev = RegressionEvaluationUtil(io_data, evaluator_params=evaluator_params)
+
+def objective(search_space_element: Dict[str, Any]):
+    log.info(f"Evaluating {search_space_element}")
+    model = create_model(search_space_element)
+    loss = ev.perform_simple_evaluation(model).get_eval_stats().compute_metric_value(metric)
+    log.info(f"Loss[{metric.name}]={loss}")
+    return {'loss': loss, 'status': hyperopt.STATUS_OK}
+```
+Following our modular design scheme, we encapsulate the metric computation
+in a high-level object `RegressionMetricRRSE`. This allows us to modify the value to be optimised
+by changing one line of code.
--- refactoring-journey/step10-hyperopt/run_regressor_hyperopt.py
+++ refactoring-journey/step10-hyperopt/run_regressor_hyperopt.py
@@ -1,6 +1,6 @@
 import os
 import warnings
-from typing import Literal
+from typing import Literal, Dict, Any
 
 import hyperopt
 from hyperopt import hp
@@ -24,7 +24,7 @@ def run_hyperopt(dataset: Dataset, model: Literal["xgb"] = "xgb"):
     logging.add_file_logger(result_writer.path("log.txt"))
 
     if model == "xgb":
-        initialSpace = [
+        initial_space = [
             {
                 'max_depth': 6,
                 'gamma': 0,
@@ -33,7 +33,7 @@ def run_hyperopt(dataset: Dataset, model: Literal["xgb"] = "xgb"):
                 'min_child_weight': 1,
             }
         ]
-        space = {
+        search_space = {
             'max_depth': hp.uniformint("max_depth", 3, 10),
             'gamma': hp.uniform('gamma', 0, 9),
             'reg_lambda': hp.uniform('reg_lambda', 0, 1),
@@ -41,18 +41,19 @@ def run_hyperopt(dataset: Dataset, model: Literal["xgb"] = "xgb"):
             'min_child_weight': hp.uniformint('min_child_weight', 1, 100),
         }
 
-        def create_model(space):
+        def create_model(search_space_element: Dict[str, Any]):
             return RegressionModelFactory.create_xgb(add_features=[FeatureName.MEAN_ARTIST_POPULARITY],
                 verbosity=0,
-                max_depth=int(space['max_depth']),
-                gamma=space['gamma'],
-                reg_lambda=space['reg_lambda'],
-                min_child_weight=int(space['min_child_weight']),
-                colsample_bytree=space['colsample_bytree'])
+                max_depth=int(search_space_element['max_depth']),
+                gamma=search_space_element['gamma'],
+                reg_lambda=search_space_element['reg_lambda'],
+                min_child_weight=int(search_space_element['min_child_weight']),
+                colsample_bytree=search_space_element['colsample_bytree'])
 
         hours = 2
         warnings.filterwarnings("ignore")
     else:
+        # Handle different models here
         raise ValueError(model)
 
     io_data = dataset.load_io_data()
@@ -60,19 +61,18 @@ def run_hyperopt(dataset: Dataset, model: Literal["xgb"] = "xgb"):
     evaluator_params = VectorRegressionModelEvaluatorParams(fractional_split_test_fraction=0.3, fractional_split_random_seed=21)
     ev = RegressionEvaluationUtil(io_data, evaluator_params=evaluator_params)
 
-    def objective(space):
-        log.info(f"Evaluating {space}")
-        model = create_model(space)
-        result = ev.perform_simple_evaluation(model)
-        loss = result.get_eval_stats().compute_metric_value(metric)
+    def objective(search_space_element: Dict[str, Any]):
+        log.info(f"Evaluating {search_space_element}")
+        model = create_model(search_space_element)
+        loss = ev.perform_simple_evaluation(model).get_eval_stats().compute_metric_value(metric)
         log.info(f"Loss[{metric.name}]={loss}")
         return {'loss': loss, 'status': hyperopt.STATUS_OK}
 
     trials_file = result_writer.path("trials.pickle")
     logging.getLogger("sensai").setLevel(logging.WARN)
     log.info(f"Starting hyperparameter optimisation for {model} and {dataset}")
-    hyperopt.fmin(objective, space, algo=hyperopt.tpe.suggest, timeout=hours*3600, show_progressbar=False,
-        trials_save_file=trials_file, points_to_evaluate=initialSpace)
+    hyperopt.fmin(objective, search_space, algo=hyperopt.tpe.suggest, timeout=hours*3600, show_progressbar=False,
+        trials_save_file=trials_file, points_to_evaluate=initial_space)
     logging.getLogger("sensai").setLevel(logging.INFO)
     trials: hyperopt.Trials = load_pickle(trials_file)
     log.info(f"Best trial: {trials.best_trial}")
