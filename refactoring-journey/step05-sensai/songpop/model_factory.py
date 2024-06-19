bd4add1474ee798c3c78d4dcb7c12d954e3f16c1
Dominik Jain <d.jain@appliedai.de>
2024-06-19 16:07:13 +0200

    Add one previous pipeline implementation of direct referencing

--- refactoring-journey/step05-sensai/songpop/model_factory.py
+++ refactoring-journey/step05-sensai/songpop/model_factory.py
@@ -2,6 +2,9 @@ from sensai.data_transformation import DFTSkLearnTransformer
 from sensai.featuregen import FeatureGeneratorTakeColumns
 from sensai.sklearn.sklearn_classification import SkLearnLogisticRegressionVectorClassificationModel, \
     SkLearnKNeighborsVectorClassificationModel, SkLearnRandomForestVectorClassificationModel, SkLearnDecisionTreeVectorClassificationModel
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 from .data import COL_YEAR, COLS_MUSICAL_DEGREES, COL_KEY, COL_MODE, COL_TEMPO, COL_TIME_SIGNATURE, COL_LOUDNESS, COL_DURATION_MS
@@ -10,6 +13,13 @@ from .data import COL_YEAR, COLS_MUSICAL_DEGREES, COL_KEY, COL_MODE, COL_TEMPO, 
 class ModelFactory:
     COLS_USED_BY_ORIGINAL_MODELS = [COL_YEAR, *COLS_MUSICAL_DEGREES, COL_KEY, COL_MODE, COL_TEMPO, COL_TIME_SIGNATURE, COL_LOUDNESS,
         COL_DURATION_MS]
+
+    # previous implementation (which is refactored below)
+    @classmethod
+    def create_logistic_regression_orig_pipeline(cls):
+        return Pipeline([
+            ("project_scale", ColumnTransformer([("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)])),
+            ("model", LogisticRegression(solver='lbfgs', max_iter=1000))])
 
     @classmethod
     def create_logistic_regression_orig(cls):
