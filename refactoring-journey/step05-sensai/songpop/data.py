aai-institute/beyond-jupyter

commit 1c5a33b28f44ec3f4f923a016f0abb7f238176db

Fix Dataset class being adapted to high-level evaluation too early

refactoring-journey/step05-sensai/songpop/data.py
@@ -1,13 +1,8 @@
-import logging
-from typing import Optional
+from typing import Tuple, Optional
 
 import pandas as pd
-from sensai import InputOutputData
-from sensai.util.string import ToStringMixin
 
 from . import config
-
-log = logging.getLogger(__name__)
 
 
 COL_POPULARITY = "popularity"
@@ -55,23 +50,20 @@ class Dataset:
         self.threshold_popular = threshold_popular
         self.drop_zero_popularity = drop_zero_popularity
         self.random_seed = random_seed
-        self.class_positive = CLASS_POPULAR
-        self.class_negative = CLASS_UNPOPULAR
 
     def load_data_frame(self) -> pd.DataFrame:
         """
         :return: the full data frame for this dataset (including the class column)
         """
-        csv_path = config.csv_data_path()
-        df = pd.read_csv(csv_path).dropna()
+        df = pd.read_csv(config.csv_data_path()).dropna()
         if self.num_samples is not None:
             df = df.sample(self.num_samples, random_state=self.random_seed)
-        determine_class = lambda x: self.class_positive if x >= self.threshold_popular else self.class_negative
-        df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(determine_class)
+        df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(lambda x: CLASS_POPULAR if x >= self.threshold_popular else CLASS_UNPOPULAR)
         return df
 
-    def load_io_data(self) -> InputOutputData:
+    def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
         """
-        :return: the I/O data
+        :return: a pair (X, y) where X is the data frame containing all attributes and y is the corresping series of class values
         """
-        return InputOutputData.from_data_frame(self.load_data_frame(), COL_GEN_POPULARITY_CLASS)
+        df = self.load_data_frame()
+        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[COL_GEN_POPULARITY_CLASS]

refactoring-journey/step06-feature-representation/songpop/data.py
@@ -1,13 +1,8 @@
-import logging
-from typing import Optional
+from typing import Tuple, Optional
 
 import pandas as pd
-from sensai import InputOutputData
-from sensai.util.string import ToStringMixin
 
 from . import config
-
-log = logging.getLogger(__name__)
 
 
 COL_POPULARITY = "popularity"
@@ -55,23 +50,20 @@ class Dataset:
         self.threshold_popular = threshold_popular
         self.drop_zero_popularity = drop_zero_popularity
         self.random_seed = random_seed
-        self.class_positive = CLASS_POPULAR
-        self.class_negative = CLASS_UNPOPULAR
 
     def load_data_frame(self) -> pd.DataFrame:
         """
         :return: the full data frame for this dataset (including the class column)
         """
-        csv_path = config.csv_data_path()
-        df = pd.read_csv(csv_path).dropna()
+        df = pd.read_csv(config.csv_data_path()).dropna()
         if self.num_samples is not None:
             df = df.sample(self.num_samples, random_state=self.random_seed)
-        determine_class = lambda x: self.class_positive if x >= self.threshold_popular else self.class_negative
-        df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(determine_class)
+        df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(lambda x: CLASS_POPULAR if x >= self.threshold_popular else CLASS_UNPOPULAR)
         return df
 
-    def load_io_data(self) -> InputOutputData:
+    def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
         """
-        :return: the I/O data
+        :return: a pair (X, y) where X is the data frame containing all attributes and y is the corresping series of class values
         """
-        return InputOutputData.from_data_frame(self.load_data_frame(), COL_GEN_POPULARITY_CLASS)
+        df = self.load_data_frame()
+        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[COL_GEN_POPULARITY_CLASS]

refactoring-journey/step07-feature-engineering/songpop/data.py
@@ -1,13 +1,8 @@
-import logging
-from typing import Optional
+from typing import Tuple, Optional
 
 import pandas as pd
-from sensai import InputOutputData
-from sensai.util.string import ToStringMixin
 
 from . import config
-
-log = logging.getLogger(__name__)
 
 
 COL_POPULARITY = "popularity"
@@ -55,23 +50,20 @@ class Dataset:
         self.threshold_popular = threshold_popular
         self.drop_zero_popularity = drop_zero_popularity
         self.random_seed = random_seed
-        self.class_positive = CLASS_POPULAR
-        self.class_negative = CLASS_UNPOPULAR
 
     def load_data_frame(self) -> pd.DataFrame:
         """
         :return: the full data frame for this dataset (including the class column)
         """
-        csv_path = config.csv_data_path()
-        df = pd.read_csv(csv_path).dropna()
+        df = pd.read_csv(config.csv_data_path()).dropna()
         if self.num_samples is not None:
             df = df.sample(self.num_samples, random_state=self.random_seed)
-        determine_class = lambda x: self.class_positive if x >= self.threshold_popular else self.class_negative
-        df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(determine_class)
+        df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(lambda x: CLASS_POPULAR if x >= self.threshold_popular else CLASS_UNPOPULAR)
         return df
 
-    def load_io_data(self) -> InputOutputData:
+    def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
         """
-        :return: the I/O data
+        :return: a pair (X, y) where X is the data frame containing all attributes and y is the corresping series of class values
         """
-        return InputOutputData.from_data_frame(self.load_data_frame(), COL_GEN_POPULARITY_CLASS)
+        df = self.load_data_frame()
+        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[COL_GEN_POPULARITY_CLASS]

refactoring-journey/step05-sensai/run_classifier_evaluation.py
@@ -8,8 +8,7 @@ from songpop.model_factory import ModelFactory
 if __name__ == '__main__':
     # define & load dataset
     dataset = Dataset(10000)
-    io_data = dataset.load_io_data()
-    X, y = io_data.inputs, io_data.outputs
+    X, y = dataset.load_xy()
 
     # split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3, shuffle=True)

refactoring-journey/step06-feature-representation/run_classifier_evaluation.py
@@ -8,8 +8,7 @@ from songpop.model_factory import ModelFactory
 if __name__ == '__main__':
     # define & load dataset
     dataset = Dataset(10000)
-    io_data = dataset.load_io_data()
-    X, y = io_data.inputs, io_data.outputs
+    X, y = dataset.load_xy()
 
     # split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3, shuffle=True)

refactoring-journey/step07-feature-engineering/run_classifier_evaluation.py
@@ -8,8 +8,7 @@ from songpop.features import FeatureName
 if __name__ == '__main__':
     # define & load dataset
     dataset = Dataset(10000)
-    io_data = dataset.load_io_data()
-    X, y = io_data.inputs, io_data.outputs
+    X, y = dataset.load_xy()
 
     # split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3, shuffle=True)

commit f5bfad5371bb4ff9aa8448a11a6883518074d1cf

Fix Dataset.load_xy returning a Series for y in steps 5, 6 and 7 while sensAI (unlike sklearn) requires a DataFrame (error was introduced in commit 1c5a33b2 in order to fix an inconsistency)

refactoring-journey/step05-sensai/songpop/data.py
@@ -63,9 +63,9 @@ class Dataset:
         df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(lambda x: CLASS_POPULAR if x >= self.threshold_popular else CLASS_UNPOPULAR)
         return df
 
-    def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
+    def load_xy(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
         :return: a pair (X, y) where X is the data frame containing all attributes and y is the corresping series of class values
         """
         df = self.load_data_frame()
-        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[COL_GEN_POPULARITY_CLASS]
+        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[[COL_GEN_POPULARITY_CLASS]]

refactoring-journey/step06-feature-representation/songpop/data.py
@@ -63,9 +63,9 @@ class Dataset:
         df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(lambda x: CLASS_POPULAR if x >= self.threshold_popular else CLASS_UNPOPULAR)
         return df
 
-    def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
+    def load_xy(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
         :return: a pair (X, y) where X is the data frame containing all attributes and y is the corresping series of class values
         """
         df = self.load_data_frame()
-        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[COL_GEN_POPULARITY_CLASS]
+        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[[COL_GEN_POPULARITY_CLASS]]

refactoring-journey/step07-feature-engineering/songpop/data.py
@@ -63,9 +63,9 @@ class Dataset:
         df[COL_GEN_POPULARITY_CLASS] = df[COL_POPULARITY].apply(lambda x: CLASS_POPULAR if x >= self.threshold_popular else CLASS_UNPOPULAR)
         return df
 
-    def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
+    def load_xy(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
         :return: a pair (X, y) where X is the data frame containing all attributes and y is the corresping series of class values
         """
         df = self.load_data_frame()
-        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[COL_GEN_POPULARITY_CLASS]
+        return df.drop(columns=COL_GEN_POPULARITY_CLASS), df[[COL_GEN_POPULARITY_CLASS]]